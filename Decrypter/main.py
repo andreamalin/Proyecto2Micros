32b3c2bb22ac2a6886f3e6d222d625e1cb3293a8
lauratamath <laurylevi@hotmail.com>
2020-09-29 12:27:57 -0600

    (:

--- Decrypter/main.py
+++ Decrypter/main.py
@@ -1,23 +1,36 @@
 from tkinter import filedialog
+#from tkinter import messagebox
 from tkinter import *
-from os import path 
+from os import path
+from PIL import Image, ImageTk
 
 # Interface
 root = Tk()
-root.title('Decrypter')
+root['bg'] = 'black'
+root.title('Message decrypter')
 root.call('wm', 'iconphoto', root._w, PhotoImage(file='icon/skull.png'))
 root.resizable(False,False)
-root.geometry('650x350')
+root.geometry('950x3500')
 frame = Frame()
 frame.pack()
-label = Label(root, text = 'DECRYPTER', font = ('Calibri','18'))
-label.place(x = 275, y = 5)
+label = Label(root, text = 'DECRYPTER', font = ('Calibri Bold','40'), fg='white', bg='black')
+label.place(x = 156, y = 5)
+
+
 
 def open_file():
-	file = filedialog.askopenfilename()
-	print(file)
+    file = filedialog.askopenfilename(filetype =(("Text files","*.txt"),("all files","*.*")))
+    print(file)
+  
+    
+    
+Button(text = 'Abrir archivo', bg='black', fg='white', command = open_file).place(x = 275,y = 120)
+imagen = ImageTk.PhotoImage(file='icon/ima.jpg')
+lbl = Label(image = imagen).place(x=79,y=205)
 
-Button(text = 'Abrir archivo', command = open_file).place(x = 10,y = 10)
+
+
+#res = messagebox.askquestion('Message','¿Seguro que quiere salir del programa?')
 
 
 root.mainloop()
