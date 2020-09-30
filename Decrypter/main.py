fc36f4528da955de1f13f187f2bfd68f38c0728b
Olivverde <dele19270@uvg.edu.gt>
2020-09-30 10:53:16 -0600

    Verificación de archivos

--- Decrypter/main.py
+++ Decrypter/main.py
@@ -3,6 +3,7 @@ from tkinter import messagebox
 from tkinter import *
 from os import path
 from PIL import Image, ImageTk
+import os
 
 # Interface
 root = Tk()
@@ -16,20 +17,45 @@ frame.pack()
 label = Label(root, text = 'DECRYPTER', font = ('Calibri Bold','40'), fg='white', bg='black')
 label.place(x = 156, y = 5)
 
+route = ''
+line = ''
 
 
 def open_file():
-    file = filedialog.askopenfilename()
-    print(file)
-    label_0 = Label(root, text = file, font = ('Calibri Bold','40'), fg='white', bg='black')
-    label_0.place(x = 300, y = 5)
+	global route
+	file = filedialog.askopenfilename(filetypes=[("Text files","*.txt")])
+	route = file
+
+def read():
+	if (route != ''):
+		global line
+		file = open(route,'r')
+		line = file.readlines()
+		print(line)
+		file.close
+		if(line != []):
+			write(line)
+		else:
+			messagebox.showinfo(message = 'Archivo vacio', title = 'Contenido faltante')
+	else:
+		messagebox.showinfo(message = 'Busque primero el archivo', title = 'Ruta faltante')
+
+def write(line):
+
+	x = line[0].split(',')
+	x.pop(len(x)-1)
+	print(x)
+
+
 
 def closing():
     if messagebox.askokcancel("Close","¿Seguro que quiere salir del programa?"):
         root.destroy()
     
     
-Button(text = 'Abrir archivo', bg='black', fg='white', command = open_file).place(x = 275,y = 120)
+Button(text = 'Abrir archivo', bg='black', fg='white', command = open_file).place(x = 275,y = 105)
+Button(text = 'Desencriptar', bg='black', fg='white', command = read).place(x = 275,y = 150)
+
 img = Image.open("icon/ima.jpg")
 img = img.resize((175,250), Image.ANTIALIAS)
 my_image =  ImageTk.PhotoImage(img)
